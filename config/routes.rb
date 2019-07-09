931f4ec9fe0bfc5d6676b4ab91637803192fef10
Aeisha Hicks <ashicks2017@outlook.com>
2019-07-09 11:00:20 -0400

    check on flyers

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :flyers
+  resources :flyers, :airports, :conversation, :meetups, :messages 
 end
+ 
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,21 +16,16 @@ Flyer.destroy_all
 
 
 
+flyer1 = Flyer.create(user_name: 'Aham', password: 'password', first_name: "Isha", last_name: "Ham", occupation: "Flight Attendant", description: "I love to travel", img_url: "https://images.pexels.com/photos/935985/pexels-photo-935985.jpeg?cs=srgb&dl=accessories-afro-beautiful-935985.jpg&fm=jpg")
 
 
-
-
-
-
-
-
-atl = Airport.create([{ name: 'Hartsfield–Jackson Atlanta International Airport', img_url: 'http://apps.atl.com/Passenger/ShopDineExplore/Default.aspx' }])
-bhm = Airport.create([{ name: 'Birmingham International Airport', img_url: 'https://www.flybirmingham.com/in-the-airport/dining/' }])
-phx = Airport.create([{ name: 'Phoenix, Phoenix Sky Harbor International Airport', img_url: 'https://www.skyharbor.com/ShopsFoodServices/Dining' }])
-lax = Airport.create([{ name: 'Los Angeles International Airport', img_url: 'https://www.flylax.com/en/lax-dining-and-shopping' }])
-oak = Airport.create([{ name: 'Oakland Airport', img_url: 'https://www.oaklandairport.com/shopping-dining-options/' }])
-san = Airport.create([{ name: 'San Diego International Airport', img_url: 'ps://www.san.org/Shop-Dine-Relax/Dining' }])
-sfo = Airport.create([{ name: 'San Francisco International Airport', img_url: 'https://www.flysfo.com/shop-dine-relax/dining/' }])
-den = Airport.create([{ name: 'Denver International Airport', img_url: 'https://www.flydenver.com/enjoy_relax/dine' }])
-lga = Airport.create([{ name: 'New York, La Guardia Airport', img_url: 'https://www.laguardiaairport.com/at-airport/shops-restaurants-and-services' }])
+atl = Airport.create( name: 'Hartsfield–Jackson Atlanta International Airport', img_url: 'http://apps.atl.com/Passenger/ShopDineExplore/Default.aspx' )
+bhm = Airport.create( name: 'Birmingham International Airport', img_url: 'https://www.flybirmingham.com/in-the-airport/dining/' )
+phx = Airport.create( name: 'Phoenix, Phoenix Sky Harbor International Airport', img_url: 'https://www.skyharbor.com/ShopsFoodServices/Dining' )
+lax = Airport.create(name: 'Los Angeles International Airport', img_url: 'https://www.flylax.com/en/lax-dining-and-shopping' )
+oak = Airport.create( name: 'Oakland Airport', img_url: 'https://www.oaklandairport.com/shopping-dining-options/' )
+san = Airport.create( name: 'San Diego International Airport', img_url: 'ps://www.san.org/Shop-Dine-Relax/Dining' )
+sfo = Airport.create( name: 'San Francisco International Airport', img_url: 'https://www.flysfo.com/shop-dine-relax/dining/' )
+den = Airport.create(name: 'Denver International Airport', img_url: 'https://www.flydenver.com/enjoy_relax/dine' )
+lga = Airport.create( name: 'New York, La Guardia Airport', img_url: 'https://www.laguardiaairport.com/at-airport/shops-restaurants-and-services' )
 
