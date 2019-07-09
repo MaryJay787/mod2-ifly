a8d4a2ff1d03e2b463d62ac2e16ad2c3a4d71a07
Aeisha Hicks <ashicks2017@outlook.com>
2019-07-09 10:06:29 -0400

    Complete Airport Controller

---
+++ app/controllers/conversation_controller.rb
@@ -0,0 +1,31 @@
+class ConversationController < ApplicationController 
+index
+    @conversation = Conversation.all
+end 
+
+def new 
+    @conversation = Conversation.new
+end 
+
+def show 
+    @conversation = Conversation.find(param[:id]) 
+end 
+
+def create
+    @conversation = Conversation.new(flyer_params)
+    if @conversation.save
+      redirect_to @conversation
+    else
+      redirect_to conversation_path
+    end
+  end
+
+  private
+
+    def conversaton_params
+      params.require(:conversation).permit(:sender_id, :recipient)
+    end
+
+end
+
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+atl = Airport.create([{ name: 'Hartsfield–Jackson Atlanta International Airport', img_url: 'http://apps.atl.com/Passenger/ShopDineExplore/Default.aspx' }])
+bhm = Airport.create([{ name: 'Birmingham International Airport', img_url: 'https://www.flybirmingham.com/in-the-airport/dining/' }])
+phx = Airport.create([{ name: 'Phoenix, Phoenix Sky Harbor International Airport', img_url: 'https://www.skyharbor.com/ShopsFoodServices/Dining' }])
+lax = Airport.create([{ name: 'Los Angeles International Airport', img_url: 'https://www.flylax.com/en/lax-dining-and-shopping' }])
+oak = Airport.create([{ name: 'Oakland Airport', img_url: 'https://www.oaklandairport.com/shopping-dining-options/' }])
+san = Airport.create([{ name: 'San Diego International Airport', img_url: 'ps://www.san.org/Shop-Dine-Relax/Dining' }])
+sfo = Airport.create([{ name: 'San Francisco International Airport', img_url: 'https://www.flysfo.com/shop-dine-relax/dining/' }])
+den = Airport.create([{ name: 'Denver International Airport', img_url: 'https://www.flydenver.com/enjoy_relax/dine' }])
+lga = Airport.create([{ name: 'New York, La Guardia Airport', img_url: 'https://www.laguardiaairport.com/at-airport/shops-restaurants-and-services' }])
